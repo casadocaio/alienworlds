9f2d1b09e1666305317bc264b00f91fc2295d4e2
Caio Tieghi <ctieghi@rv3ti.com>
2022-02-24 17:42:16 -0300

    fix: retirados comps demais e adicionado dice

--- src/components/TLM.js
+++ src/components/TLM.js
@@ -11,6 +11,11 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
     const [lastActionsCAIT, setLastActionsCAIT] = useState([{}]);
     const [lstMediaCAIT, setLstMediaCAIT] = useState([{}]);
 
+    const [rowsDICE, setRowsDICE] = useState([]);
+    const [queryJsonDICE, setQueryJsonDICE] = useState([]);
+    const [lastActionsDICE, setLastActionsDICE] = useState([{}]);
+    const [lstMediaDICE, setLstMediaDICE] = useState([{}]);
+
     function getDiffMinutes(d) {
         return Math.floor(((Date.now() - d) / 1000) / 60)
     }
@@ -73,6 +78,29 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
         setLastActionsCAIT(tratado);
 
     }, [queryJsonCAIT]);
+
+    useEffect(() => {
+        let tratado = [];
+
+        //console.log('veio montar tratado');
+
+        if (queryJsonDICE && queryJsonDICE[0] && queryJsonDICE[0].act) {
+            tratado = queryJsonDICE.map(q => {
+                let data_corrigida = new Date(new Date(q.timestamp).setHours(new Date(q.timestamp).getHours() - (new Date(q.timestamp).getTimezoneOffset() / 60)))
+
+                return {
+                    timestamp: q.timestamp,
+                    time: data_corrigida,
+                    symbol: q.act.data.symbol,
+                    quantity: q.act.data.quantity,
+                    memo: q.act.data.memo,
+                }
+            });
+        }
+        //console.log('veio montar tratado', tratado);
+        setLastActionsDICE(tratado);
+
+    }, [queryJsonDICE]);
 
     useEffect(() => {
         let lastSnake = [];
@@ -211,6 +239,74 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
 
     }, [lastActionsCAIT]);
 
+    useEffect(() => {
+        let lastSnake = [];
+
+        if (lastActionsDICE) {
+            if (lastActionsDICE[0]) {
+                if (lastActionsDICE[0].symbol) {
+                    lastActionsDICE.forEach(la => {
+                        if (la.symbol === "DUST" && la.memo.includes("DU$T Dice")) {
+                            lastSnake.push({
+                                symbol: la.symbol,
+                                quantity: la.quantity.replace(' DUST', ''),
+                                time: la.time,
+                                day: la.time.getDate(),
+                                hora: la.time.getHours(),
+                                min: la.time.getMinutes(),
+                                diff: getDiffMinutes(la.time),
+                                completo: la,
+                            })
+                        }
+                    })
+                }
+            }
+        }
+
+        if (lastSnake[0]) {
+            //console.log('ver lastSnake', lastSnake);
+
+            let dias = [];
+            let consolidado = [];
+
+            lastSnake.forEach(ls => {
+                if (!dias.includes(ls.day)) {
+                    dias.push(ls.day);
+                }
+            });
+
+            //console.log('ver dias', dias);
+
+            dias.forEach(d => {
+                let filtrado = dadosDia(lastSnake, d);
+
+                let qtd = 0.0;
+                let sum = 0.0;
+                let min = 2.0;
+                let max = 0.0;
+                let avg = 0.0;
+
+                filtrado.forEach(f => {
+                    qtd += 1;
+                    sum = sum + parseFloat(f.quantity);
+                    min = min > parseFloat(f.quantity) ? parseFloat(f.quantity) : min;
+                    max = max < parseFloat(f.quantity) ? parseFloat(f.quantity) : max;
+                    avg = sum / qtd;
+                });
+
+                consolidado[dias.indexOf(d)] = { day: d, qtd: qtd, sum: sum, min: min, max: max, avg: avg }
+            });
+
+            //console.log('ver consolidado', consolidado);
+
+            setLstMediaDICE(consolidado)
+
+        }
+
+        //console.log('lastSnake', lastSnake);
+
+    }, [lastActionsDICE]);
+
     function dadosDia(dados, dia) {
         return dados.filter(dd => {
             if (dd.day === dia) {
@@ -268,7 +364,7 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
 
         //console.log('lstMediaCAIT', lstMediaCAIT);
 
-        if (lstMediaCAIT && lstMediaCAIT.length > 1) {
+        if (lstMediaCAIT && lstMediaCAIT.length > 0) {
             for (var i = 0; i < lstMediaCAIT.length; i++) {
                 let rowID = `row${i + 1}`
                 let cell = []
@@ -285,6 +381,39 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
         setRowsCAIT(tmprows);
 
     }, [lstMediaCAIT]);
+
+    useEffect(() => {
+
+        let tmprows = [];
+
+        let cabecalho = []
+        cabecalho.push(<th key={`cell${0}-${1}`} id={`cell${0}-${1}`}>Dia</th>)
+        cabecalho.push(<th key={`cell${0}-${2}`} id={`cell${0}-${2}`}>Payouts</th>)
+        cabecalho.push(<th key={`cell${0}-${3}`} id={`cell${0}-${3}`}>Total</th>)
+        cabecalho.push(<th key={`cell${0}-${4}`} id={`cell${0}-${4}`}>Média</th>)
+        cabecalho.push(<th key={`cell${0}-${5}`} id={`cell${0}-${5}`}>Max</th>)
+        cabecalho.push(<th key={`cell${0}-${6}`} id={`cell${0}-${6}`}>Min</th>)
+        tmprows.push(<tr key={0} id={`row${0}`} className='cabecalho'>{cabecalho}</tr>)
+
+        //console.log('lstMediaDICE', lstMediaDICE);
+
+        if (lstMediaDICE && lstMediaDICE.length > 0) {
+            for (var i = 0; i < lstMediaDICE.length; i++) {
+                let rowID = `row${i + 1}`
+                let cell = []
+                cell.push(<td key={`cell${i + 1}-${1}`} id={`cell${i + 1}-${1}`}>{lstMediaDICE[i].day}</td>)
+                cell.push(<td key={`cell${i + 1}-${2}`} id={`cell${i + 1}-${2}`}>{lstMediaDICE[i].qtd}</td>)
+                cell.push(<td key={`cell${i + 1}-${3}`} id={`cell${i + 1}-${3}`}>{Math.round(lstMediaDICE[i].sum * 100) / 100}</td>)
+                cell.push(<td key={`cell${i + 1}-${4}`} id={`cell${i + 1}-${4}`}>{Math.round(lstMediaDICE[i].avg * 100) / 100}</td>)
+                cell.push(<td key={`cell${i + 1}-${5}`} id={`cell${i + 1}-${5}`}>{lstMediaDICE[i].max}</td>)
+                cell.push(<td key={`cell${i + 1}-${6}`} id={`cell${i + 1}-${6}`}>{lstMediaDICE[i].min}</td>)
+                tmprows.push(<tr key={i + 1} id={rowID}>{cell}</tr>)
+            }
+        }
+
+        setRowsDICE(tmprows);
+
+    }, [lstMediaDICE]);
 
     function onClick(){
 
@@ -324,7 +453,7 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
        }
    }
 
-   function onClickCAIT(){
+function onClickCAIT(){
 
     try {
        if (userAccount) {
@@ -339,6 +468,28 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
                .then(data => {
                    setQueryJsonCAIT(data.actions)
                    //console.log('data.actions', data.actions)
+               });
+       }
+   } catch(e) {
+       console.log('error: ' + e.message);
+   }
+}
+
+function onClickDICE(){
+
+    try {
+       if (userAccount) {
+           let qtd =1000;
+
+           fetch('https://api.waxsweden.org/v2/history/get_actions?limit=' + qtd + '&filter=niftywizards%3A*&skip=0&account=' + userAccount + '&sort=desc'
+           /*, {
+               method: 'GET', // *GET, POST, PUT, DELETE, etc.
+               referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+             }*/)
+               .then(response => response.json())
+               .then(data => {
+                   setQueryJsonDICE(data.actions)
+                   //console.log('dice data.actions', data.actions)
                });
        }
    } catch(e) {
@@ -363,6 +514,9 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
                                 <button className="btnAtualizar" onClick={onClickCAIT} >
                                     CAIT
                                 </button>
+                                <button className="btnAtualizar" onClick={onClickDICE} >
+                                    DU$T DICE
+                                </button>
                             </p>
                             <table id="simple-board">
                                 <tbody>
@@ -373,6 +527,13 @@ function TLM({ userAccount, queryJson, setQueryJson }) {
                             <table id="simple-boardCait">
                                 <tbody>
                                     {rowsCAIT}
+                                </tbody>
+                            </table>
+                            </p>
+                            <p>
+                            <table id="simple-boardDice">
+                                <tbody>
+                                    {rowsDICE}
                                 </tbody>
                             </table>
                             </p>
